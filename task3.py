e98a51ef53d023ac57bbbf341ba460ad1ed74ce0
shmatov-nikolai <grekkg@gmail.com>
2020-02-17 03:42:45 +0600

    peredelka

--- task3.py
+++ task3.py
@@ -1,5 +1,6 @@
 '''
-3. Hужно нарисовать на экране доску для игры в крестики нолики. Пользователь вводит размер доски (3х3, 4х4 и т.д.)
+3. Hужно нарисовать на экране доску для игры в крестики нолики.
+Пользователь вводит размер доски (3х3, 4х4 и т.д.)
 и после этого ваши программа выводит её на экран.
 Например:
 Введите размер: 3 3
@@ -13,22 +14,22 @@
 --- --- ---
 '''
 
+
 def kvadrat_setka(stroka, stolbec):
-    i=0
+    i = 0
     while i < stroka:
         if stroka == stolbec:
-            print("--- " * (stroka))
+            print(" ---" * (stroka))
             print("|   " * (stolbec + 1))
-            i+=1
-            if i ==stroka:
-                print("--- " * (stolbec))
+            i += 1
+            if i == stroka:
+                print(" ---" * (stolbec))
         else:
-            print("--- " * (stroka - (stroka - stolbec)))
+            print(" ---" * (stroka - (stroka - stolbec)))
             print("|   " * (stolbec + 1))
-            i+=1
-            if i ==stroka:
-                print("--- " * (stroka - (stroka - stolbec)))
-        
+            i += 1
+            if i == stroka:
+                print(" ---" * (stroka - (stroka - stolbec)))
 
 
-kvadrat_setka(3, 1)
+kvadrat_setka(1, 1)
